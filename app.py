5c1da4020414f1eb5535b8ee89fd1e6b35c1c667
jselvan161293 <janani.selvan@colorado.edu>
2020-03-02 18:14:20 -0700

    Creating a simple HTML form

--- app.py
+++ app.py
@@ -1,9 +1,31 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/heart-beat')
 def hello():
     return 'Hello, World!'
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'Welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+
+if __name__ == "__main__":
+    app.run()
 
