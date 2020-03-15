64909f47d952ab615ebfc205640485c17f3d8dfb
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2020-03-14 23:25:45 -0600

    Remove database test endpoint

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, redirect, url_for, request, render_template
-import os
-import pymongo
 
 app = Flask(__name__)
 
@@ -26,18 +24,6 @@ def login():
     else:
         user = request.args.get('nm')
         return redirect(url_for('success', name=user))
-
-@app.route('/dbTest')
-def db_test():
-    user = os.environ["DB_USER"]
-    pwd = os.environ["DB_PASS"]
-    db_addr = os.environ["DB_ADDR"]
-    db_name = os.environ["DB_NAME"]
-    
-    client = pymongo.MongoClient("mongodb+srv://" + user + ":" + pwd + "@" + db_addr)
-    db = client[db_name]
-
-    return str([doc for doc in db.foo.find()])
 
 if __name__ == "__main__":
     app.run()
