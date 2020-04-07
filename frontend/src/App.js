683269d2b17a526a3a8c191cfd34947a6831b130
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2020-04-07 02:25:33 -0600

    Add sign up form to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-import Login from './Login';
+import Auth from './Auth';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <Login></Login>
+      <Auth></Auth>
     </header>
     </div>
   );
