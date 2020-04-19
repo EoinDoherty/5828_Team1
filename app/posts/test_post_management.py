a0648ec996ea5b72ae97407faadb6eb76c8896f4
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2020-04-19 13:58:29 -0600

    Add post management tests

--- app/posts/test_post_management.py
+++ app/posts/test_post_management.py
@@ -1,6 +1,6 @@
 from app import app
 from app.posts.post_management import *
-from mock import patch
+from mock import patch, MagicMock
 from bson import ObjectId
 
 context = "app.posts.post_management"
@@ -13,7 +13,7 @@ def test_list_posts():
                    "creator": "user",
                    "title": "title",
                    "content": "content"}
-            mock.posts.find_one.find = [doc]
+            mock.posts.find.return_value = [doc]
                                          
             result = list_posts("user")
 
@@ -28,8 +28,66 @@ def test_list_posts():
             assert first["_id"] == str(oid)
         
         with patch(context + ".database") as mock:
-            mock.posts.find_one = []
+            mock.posts.find.return_value = []
             result = list_posts("user")
 
             assert type(result) == list
             assert len(result) == 0
+
+def test_get_post():
+    with app.app_context():
+        with patch(context + ".database") as mock:
+            oid = ObjectId()
+            doc = {"_id": oid}
+            mock.posts.find_one.return_value = doc
+
+            result = get_post("user", str(oid))
+
+            assert type(result) == dict
+            assert result["_id"] == str(doc["_id"])
+        with patch(context + ".database") as mock:
+            mock.posts.find_one.return_value = None
+
+            result = get_post("user", str(ObjectId()))
+
+            assert result == None
+
+def test_create_post():
+    with app.app_context():
+        with patch(context + ".database") as mock:
+            oid = ObjectId()
+
+            insert_ack = MagicMock()
+            insert_ack.inserted_id = oid
+            mock.posts.insert_one.return_value = insert_ack
+            result = create_post("user", "title", "content")
+            assert result == str(oid)
+
+def test_update_post():
+    with app.app_context():
+        with patch(context + ".database") as mock:
+            oid = ObjectId()
+            mock.posts.find_one.return_value = {"_id": oid, "creator": "user"}
+            mock.posts.replace_one.return_value = {"_id": oid}
+            result = update_post("user", str(oid), "title", "content")
+            assert result
+        with patch(context + ".database") as mock:
+            mock.posts.find_one.return_value = None
+            result = update_post("user", str(ObjectId()), "title", "content")
+            assert not result
+
+def test_delete_post():
+    with app.app_context():
+        oid = ObjectId()
+        with patch(context + ".database") as mock:
+            mock.posts.find_one.return_value = {"_id": oid, "creator": "user"}
+            mock.posts.remove.return_value = {"_id": oid, "creator": "user"}
+            result = delete_post("user", str(oid))
+            assert result == OK
+            
+            result = delete_post("not user", str(oid))
+            assert result == UNAUTHORIZED
+
+            mock.posts.remove.return_value = None
+            result = delete_post("user", str(oid))
+            assert result == NOT_FOUND
