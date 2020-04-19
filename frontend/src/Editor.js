5ce30647010835dc15c9c78f503aa89f41eed2f8
Eoin Doherty <EoinDoherty@users.noreply.github.com>
2020-04-19 02:16:20 -0600

    Add post CRUD functionality to react UI

--- frontend/src/Editor.js
+++ frontend/src/Editor.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Home from './Home';
 
 function Editor (props) {
 
     const apiHeaders = props["apiHeaders"];
+    let [postId, setPostId] = useState(props.postId);
+    let [exit, setExit] = useState(false);
 
     function savePost() {
         const title = document.getElementById("post-title").value;
         const content = document.getElementById("post-editor").value;
-
-        console.log(title);
-        console.log(content);
         
-        const update_obj = {
-            "title": title,
-            "content": content
-        };
-        
-        // Send object here
-
+        if (postId) {
+            // Overwrite an existing post
+            fetch("/api/update_post", {
+                method: 'POST',
+                headers: apiHeaders,
+                body: JSON.stringify({
+                    "title": title,
+                    "content": content,
+                    "id": postId
+                })
+            })
+        } else {
+            // Create a new post
+            fetch("/api/new_post", {
+                method: 'POST',
+                headers: apiHeaders,
+                body: JSON.stringify({
+                    "title": title,
+                    "content": content
+                })
+            }).then(response => {
+                if (response.status === 200) {
+                    response.json()
+                        .then(data => 
+                            setPostId(data.id)
+                    )
+                }
+            })
+        }
     }
 
     function goHome() {
-        console.log("Get out of here");
+        setExit(true);
+    }
+
+    let title = props.title ? props.title : "";
+    let content = props.content ? props.content : "";
+
+    if (exit) {
+        return <Home token={props.token} apiHeaders={props.apiHeaders}/>
     }
 
     return (
         <div className="editor">
             <h3>Title:</h3>
-            <input type="text" id="post-title" name="title"></input>
+            <input type="text" id="post-title" name="title" defaultValue={title}></input>
             <br></br>
             <h3>Content:</h3>
-            <textarea id="post-editor" rows="5" cols="100"></textarea>
+            <textarea id="post-editor" rows="5" cols="100" defaultValue={content}></textarea>
             <br></br>
             <button id="save-post" onClick={savePost}>Save</button>
             <br></br>
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,41 +1,93 @@
-import React, { useState } from 'react';
-import Auth from './Auth'
+import React, { useState, useEffect } from 'react';
 import Editor from './Editor';
 
 function Home (props) {
 
     const [editorObj, setEditorObj] = useState(undefined);
+    const [username, setUsername] = useState("");
+
+    useEffect(() => {
+        let options = {method: 'GET', headers: props.apiHeaders};
+        fetch("/api/get_username", options)
+            .then(response => {
+                response.json().then(data => 
+                    setUsername(data.msg)
+                );
+            })
+        },
+    [props.apiHeaders]);
 
     function openEditor() {
-        setEditorObj(<Editor token={props["tokens"]} apiHeaders={apiHeaders}></Editor>);
+        setEditorObj(<Editor token={props["token"]} apiHeaders={props["apiHeaders"]}></Editor>);
     }
 
-    const apiHeaders = props["apiHeaders"];
+    function editExisting(post) {
+        setEditorObj(<Editor token={props["token"]} 
+                             apiHeaders={props["apiHeaders"]} 
+                             title={post.title} 
+                             content={post.content} 
+                             postId={post._id}>
+                    </Editor>)
+    }
+
+    function deletePost(postId) {
+        fetch("/api/delete_post", {
+            method: 'POST',
+            headers: props.apiHeaders,
+            body: JSON.stringify({"id": postId})
+        }).then(request => 
+            fetchPosts()
+        );
+    }
+
+    // const apiHeaders = props["apiHeaders"];
 
     const [status, setStatus] = useState(404);
-    const [message, setMessage] = useState("Unable to connect to backend server");
+    // const [message, setMessage] = useState("Unable to connect to backend server");
+    const [posts, setPosts] = useState([]);
 
-    const requestOptions = {
-        method: 'GET',
-        headers: apiHeaders
-    };
+    // fetch('api/home', requestOptions)
+    //     .then(response => {
+    //         setStatus(response.status);
+    //         return response.json()})
+    //     .then(data => setMessage(data["msg"]));
 
-    fetch('api/home', requestOptions)
-        .then(response => {
-            setStatus(response.status);
-            return response.json()})
-        .then(data => setMessage(data["msg"]));
+    function fetchPosts() {
+        const requestOptions = {
+            method: 'GET',
+            headers: props.apiHeaders
+        }
+        fetch('api/list_posts', requestOptions)
+            .then(response => {
+                setStatus(response.status);
+                response.json()
+                .then(data =>
+                    setPosts(data.posts)
+                )
+            })
+    }
+
+    useEffect(fetchPosts, [props.apiHeaders]);
 
     if (status === 200) {
         if (editorObj !== undefined) {
-            console.log("ok")
             return <div>{editorObj}</div>
         }
         return (
             <div>
-                <h3>Success!</h3>
+                <h3>Hello {username}</h3>
                 <h3>Posts:</h3>
-                {/* TODO: Put post list from api here */}
+                <ul>
+                    {posts.map((post, index) => 
+                        <div key={index}  post-id={post["_id"]}>
+                            <li>
+                                {post.title}
+                                <button onClick={() => editExisting(post)}>Edit</button>
+                                <button onClick={() => deletePost(post._id)}>Delete</button>
+                            </li>
+                        </div>
+                    )}
+                </ul>
                 <button onClick={openEditor}>Create a new post</button>
             </div>
         );
